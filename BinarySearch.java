276ae6f82b06ed73b4f1b78f898ccae37c404d8d
Owen Wang <owenwang0099@gmail.com>
2022-09-28 15:05:58 -0400

    hehehe

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,13 +3,11 @@ import java.util.Arrays;
 public class BinarySearch {
     public static int binarySearch(int[] list, int item) {
         int l = 0, r = list.length - 1, m;
-        boolean found = false;
         Arrays.sort(list);
 
-        while (l <= r && !found) {
+        while (l <= r) {
             m = (l + r) / 2;
             if (list[m] == item) {
-                found = true;
                 return m;
             }
             else if (item > list[m]) {
@@ -24,13 +22,11 @@ public class BinarySearch {
 
     public static int binarySearchString(String[] list, String item) {
         int l = 0, r = list.length - 1, m;
-        boolean found = false;
         Arrays.sort(list);
 
-        while (l <= r && !found) {
+        while (l <= r) {
             m = (l + r) / 2;
             if (list[m].equals(item)) {
-                found = true;
                 return m;
             }
             else if (item.compareTo(list[m]) > 0) {
@@ -45,12 +41,10 @@ public class BinarySearch {
 
     public static int binarySearchDescending(int[] list, int item) {
         int l = 0, r = list.length - 1, m;
-        boolean found = false;
 
-        while (l <= r && !found) {
+        while (l <= r) {
             m = (l + r) / 2;
             if (list[m] == item) {
-                found = true;
                 return m;
             }
             else if (item < list[m]) {
@@ -61,6 +55,27 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    public static int binarySearchNearest(int[] list, int item) {
+        int l = 0, r = list.length - 1, m;
+
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (list[m] == item) {
+                return m;
+            }
+            else if (item < list[m]) {
+                l = m + 1;
+            }
+            else {
+                r = m - 1;
+            }
+        }
+        if (item - list[l] < item - list[r]) {
+            return list[l];
+        }
+        return list[r];
     }
 
     public static void main(String[] args) {
