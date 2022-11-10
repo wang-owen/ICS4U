567162ea3255e4e6120362bc089104db894f8fdc
Owen Wang <owenwang0099@gmail.com>
2022-11-10 14:52:45 -0500

    teehee

---
+++ OOP/Classes/Computer.java
@@ -0,0 +1,49 @@
+public class Computer {
+    private String manufacturer, serial;
+    private int year_made, year_purchased, ram, warranty_expiration;
+    private double speed;
+
+    public Computer(String manufacturer, String serial, double speed, int ram, int year_made, int year_purchased,
+            int warranty_expiration) {
+        this.manufacturer = manufacturer;
+        this.serial = serial;
+        this.speed = speed;
+        this.ram = ram;
+        this.year_made = year_made;
+        this.year_purchased = year_purchased;
+        this.warranty_expiration = warranty_expiration;
+    }
+
+    public String toString() {
+        return String.format("Manufacturer: %s\nSerial: %d\nProcessor Speed: %.1f\nRAM: %d\nYear Made: %d\nYear Purchased: %d\nWarranty Expiration: %d\n", manufacturer, serial, speed, ram, year_made, year_purchased, warranty_expiration);
+    }
+
+    public Computer compareRAM(Computer other) {
+        if (other.ram > this.ram) {
+            return other;
+        }
+        return this;
+    }
+
+    public int getYearMade() {
+        return year_made;
+    }
+
+    public int getRAM() {
+        return ram;
+    }
+
+    public Computer compareSpeed(Computer other) {
+        if (other.speed > this.speed) {
+            return other;
+        }
+        return this;
+    }
+
+    public Computer compareAge(Computer other) {
+        if (other.year_made < this.year_made) {
+            return other;
+        }
+        return this;
+    }
+}
---
+++ OOP/Classes/Lab.java
@@ -0,0 +1,63 @@
+public class Lab {
+    private int max_capacity, num_installed;
+    private Computer[] computers;
+
+    public Lab(int max_capcity) {
+        this.max_capacity = max_capacity;
+        computers = new Computer[max_capacity];
+        num_installed = 0;
+    }
+
+    public void installComputer(int serial, double speed, int ram, int year_made, int year_purchased,
+            int warranty_expiration) {
+        if (num_installed >= max_capacity) {
+            System.out.println("Cannot install more computers.");
+        } else {
+            Computer computer = new Computer(serial, speed, ram, year_made, year_purchased, warranty_expiration);
+            computers[num_installed] = computer;
+            num_installed++;
+        }
+    }
+
+    public int avgAge() {
+        int age = 0;
+        for (int i = 0; i < num_installed; i++) {
+            age += 2022 - computers[i].getYearMade();
+        }
+        return age / num_installed;
+    }
+
+    public Computer newestComputer() {
+        Computer latestComputer = computers[0];
+        int latest = computers[0].getYearMade();
+        for (int i = 0; i < num_installed; i++) {
+            if (computers[i].getYearMade() > latest) {
+                latestComputer = computers[i];
+                latest = computers[i].getYearMade();
+            }
+        }
+        return latestComputer;
+    }
+
+    public Computer fastestComputer() {
+        Computer fastestComputer = computers[0];
+        for (int i = 0; i < num_installed - 1; i++) {
+                fastestComputer = computers[i].compareSpeed(computers[i + 1]);
+            }
+        return fastestComputer;
+    }
+
+    public Computer highestRAM() {
+        Computer fastestRAM = computers[0];
+        int fastest = computers[0].getRAM();
+        for (int i = 0; i < num_installed; i++) {
+            if (computers[i].getRAM() > fastest) {
+                fastestRAM = computers[i];
+                fastest = computers[i].getRAM();
+            }
+        }
+        return fastestRAM;
+    }
+
+    
+}
